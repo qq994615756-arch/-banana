020e42ec7d8d179c149bd7912dcc22c6123b7ad3
zzzzzzJ <qq994615756@gmail.com>
2026-04-15 13:56:33 +0800

    Update layout.tsx

--- banana/app/layout.tsx
+++ banana/app/layout.tsx
@@ -1,22 +1,9 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        {/* 新增这一层，填入你在 Google Cloud 申请的 Client ID */}
-        <GoogleOAuthProvider clientId="你的_GOOGLE_CLIENT_ID.apps.googleusercontent.com">
-           {children}
-        </GoogleOAuthProvider>
-      </body>
-    </html>
-  )
-}
 import type { Metadata, Viewport } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from "next-themes"
 import { Toaster } from "sonner"
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import "./globals.css"
 
 const geistSans = Geist({
@@ -50,22 +37,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster 
-            position="top-center" 
-            richColors 
-            closeButton
-            toastOptions={{
-              duration: 3000,
-            }}
-          />
-        </ThemeProvider>
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={false}
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster 
+              position="top-center" 
+              richColors 
+              closeButton
+              toastOptions={{
+                duration: 3000,
+              }}
+            />
+          </ThemeProvider>
+        </GoogleOAuthProvider>
         {process.env.NODE_ENV === "production" && <Analytics />}
       </body>
     </html>
